13165db97b6ec3cdf10f3afe930a87960a1a7000
Ammar Ahmad Awan <ammar.awan@microsoft.com>
2022-07-11 15:10:47 -0700

    Delete download_books.sh

--- download_books.sh
+++
@@ -1,2 +0,0 @@
-wget https://mystic.the-eye.eu/public/AI/pile_neox/data/BookCorpusDataset_text_document.idx
-wget https://mystic.the-eye.eu/public/AI/pile_neox/data/BookCorpusDataset_text_document.bin
