f571a25aec047d5863aaee1e7bb95ab1197b2d9b
iLeGend <youzhi.jin@intel.com>
2024-04-11 17:15:50 +0800

    Support Llama2Tokenizer

--- megatron/arguments.py
+++ megatron/arguments.py
@@ -1273,6 +1273,7 @@ def _add_data_args(parser):
                                 'GPT2BPETokenizer',
                                 'SentencePieceTokenizer',
                                 'GPTSentencePieceTokenizer',
+                                'Llama2Tokenizer',
                                 'HFTokenizer',
                                 'NullTokenizer'],
                        help='What type of tokenizer to use.')
